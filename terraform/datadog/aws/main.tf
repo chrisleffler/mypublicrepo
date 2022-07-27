10f992160eeda44951a4ccfe80dda81bd10b1e9f
chrisleffler <chris.leffler@datadoghq.com>
2022-07-27 13:53:02 -0400

    vector and k8s updates

--- terraform/datadog/aws/main.tf
+++ terraform/datadog/aws/main.tf
@@ -2,4 +2,7 @@
 resource "datadog_integration_aws" "sandbox" {
   access_key_id = "1234567891234567"
   secret_access_key = var.aws_secret_access_key
+  host_tags = ["env:<ENVIRONMENT>","cloud_provider:aws","aws_account:<AWS_ACCOUNT_ID>"]
+  ## (List of String) An array of AWS regions to exclude from metrics collection.
+  # excluded_regions = []
 }
