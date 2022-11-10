f9f8163e265832dddf73e87159bec89ff2b47cbf
chrisleffler <chris.leffler@datadoghq.com>
2022-11-10 15:49:27 -0500

    added comments

--- terraform/datadog/synthetics/ssl/main.tf
+++ terraform/datadog/synthetics/ssl/main.tf
@@ -18,7 +18,7 @@ resource "datadog_synthetics_test" "test_ssl" {
     operator = "isInMoreThan"
     target   = 14
   }
-  locations = ["aws:us-east-1"]
+  locations = ["aws:us-east-1"] #Update with one or more public or private locations. Private locations identifiers can be found here -- https://app.datadoghq.com/synthetics/settings/private-locations by click on the location name and then opening the Metadata tab.
   options_list {
     tick_every         = 86400 #Test interval time in seconds
     monitor_name = "Datadog : Host : ${each.key} : Certificate expiring < 14"
